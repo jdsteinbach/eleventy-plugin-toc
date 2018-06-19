0ce399c691a122b01680e00380cbeeb3b7f3dc39
James Steinbach <jdsteinbach@gmail.com>
2018-06-19 17:41:54 -0600

    initial commit
    2-deep; needs recursion

---
+++ src/BuildList.js
@@ -0,0 +1,17 @@
+const BuildLink = ({id, text, children}) => {
+  const nestedList = children.length > 0
+    ? BuildList(children)
+    : ''
+  return ( id && text )
+    ? `<li><a href="#${id}">${text}</a>${nestedList}</li>`
+    : ''
+}
+
+const BuildList = (listItems) => {
+  const list = listItems.map(li => BuildLink(li))
+  return ( list.length > 0 )
+    ? `<ol>${list.join('')}</ol>`
+    : ''
+}
+
+module.exports = BuildList
