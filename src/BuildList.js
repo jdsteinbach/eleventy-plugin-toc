537f614bd11baf4e1bc6eb77cb6ff9428081cf4e
James Steinbach <jdsteinbach@gmail.com>
2018-06-22 10:25:23 -0600

    it works recursively now

--- src/BuildList.js
+++ src/BuildList.js
@@ -2,13 +2,17 @@ const BuildLink = ({id, text, children}) => {
   const nestedList = children.length > 0
     ? BuildList(children)
     : ''
+
   return ( id && text )
     ? `<li><a href="#${id}">${text}</a>${nestedList}</li>`
     : ''
 }
 
 const BuildList = (listItems) => {
-  const list = listItems.map(li => BuildLink(li))
+  const list = listItems
+    .sort((a, b) => a.order > b.order)
+    .map(li => BuildLink(li))
+
   return ( list.length > 0 )
     ? `<ol>${list.join('')}</ol>`
     : ''
