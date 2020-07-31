94ad72e8e773246195dfa34f4d00c02d1c4eb45b
James Steinbach <jdsteinbach@gmail.com>
2020-07-31 09:44:49 -0600

    Escape HTML Entities in link text (prevent HTML markup generation)

--- src/BuildList.js
+++ src/BuildList.js
@@ -1,17 +1,26 @@
-const BuildLink = ({id, text, children}) => {
+// Replace list copied from https://css-tricks.com/snippets/javascript/htmlentities-for-javascript/
+const _escText = text => {
+  return String(text)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+const _buildLink = ({id, text, children}) => {
   const nestedList = children.length > 0
     ? BuildList(children)
     : ''
 
   return ( id && text )
-    ? `<li><a href="#${id}">${text}</a>${nestedList}</li>`
+    ? `<li><a href="#${id}">${_escText(text)}</a>${nestedList}</li>`
     : ''
 }
 
 const BuildList = (listItems) => {
   const list = listItems
     .sort((a, b) => a.order > b.order)
-    .map(li => BuildLink(li))
+    .map(li => _buildLink(li))
 
   return ( list.length > 0 )
     ? `<ol>${list.join('')}</ol>`
