bc21cccdacc173181aa333a379147c0557fd6210
Michael Solati <mkslt04@gmail.com>
2020-08-05 12:04:18 -0700

    refactor: reimplement `namespace`

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 const buildTOC = require('./src/BuildTOC')
 const parseOptions = require('./src/ParseOptions')
 
-module.exports = (eleventyConfig, globalOpts = {}) => {
-  eleventyConfig.addFilter('toc', (content, localOpts) => {
-    return buildTOC(content, parseOptions(localOpts, globalOpts))
+module.exports = (eleventyConfig, globalOpts) => {
+  globalOpts = globalOpts || {}
+  eleventyConfig.namespace(globalOpts, () => {
+    eleventyConfig.addFilter('toc', (content, localOpts) => {
+      return buildTOC(content, parseOptions(localOpts, globalOpts))
+    })
   })
 }
