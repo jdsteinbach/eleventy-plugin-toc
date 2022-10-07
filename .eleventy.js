a644f405efb26523546ed41f09e66355475a09f0
Benny Powers <web@bennypowers.com>
2022-10-07 10:36:44 +0300

    feat: wrapper as function

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,8 @@ const parseOptions = require('./src/ParseOptions')
 module.exports = (eleventyConfig, globalOpts) => {
   globalOpts = globalOpts || {}
   eleventyConfig.namespace(globalOpts, () => {
-    eleventyConfig.addFilter('toc', (content, localOpts) => {
-      return buildTOC(content, parseOptions(localOpts, globalOpts))
+    eleventyConfig.addFilter('toc', function (content, localOpts) {
+      return buildTOC(content, parseOptions.call(this, localOpts, globalOpts))
     })
   })
 }
